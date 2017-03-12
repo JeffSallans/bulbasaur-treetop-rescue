b425dcdea3998a56b59f20250cdeae4464feebd2
mlripper <mlripper33@gmail.com>
2017-03-11 19:24:48 -0500

    Progress on hook raycasting

--- Assets/Player/Scripts/Hook.cs
+++ Assets/Player/Scripts/Hook.cs
@@ -18,7 +18,14 @@ public class Hook : MonoBehaviour
     private PlayerData player;
     private Rigidbody myRidgidbody;
 
-    public Rigidbody vineRigidbody;
+    public GameObject crossHairs;
+    public GameObject camera;
+
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+    int hookMask;
+    int RayLength = 100;
+    //public Rigidbody vineRigidbody;
 
     public LineRenderer lineRenderer;
 
@@ -27,6 +34,7 @@ public class Hook : MonoBehaviour
     {
         myRidgidbody = gameObject.GetComponent<Rigidbody>();
         player = gameObject.GetComponent<PlayerData>();
+        hookMask = LayerMask.GetMask("Hook");
     }
 
     // Update is called once per frame
@@ -38,17 +46,32 @@ public class Hook : MonoBehaviour
         //var currentHindgeJoint = gameObject.GetComponent<HingeJoint>();
 
         // Instantiated Vine
-        Rigidbody vine = null;
-        Vector3 offset = hookTarget.gameObject.transform.position - gameObject.transform.position;
+        //Rigidbody vine = null;
+        //Vector3 offset = hookTarget.gameObject.transform.position - gameObject.transform.position;
 
         var fixedJoint = hookTarget.GetComponent<FixedJoint>();
 
         if (input.getSecondaryActionPressDown())
         {
             float dist = Vector3.Distance(hookTarget.gameObject.transform.position, gameObject.transform.position);
-            Debug.Log(dist);
-            Debug.Log(myRidgidbody.velocity.normalized.ToString());
-            myRidgidbody.AddForce(myRidgidbody.velocity.normalized * Time.deltaTime * player.attachForceMag);
+            //Debug.Log(dist);
+            //Debug.Log(myRidgidbody.velocity.normalized.ToString());
+            //myRidgidbody.AddForce(myRidgidbody.velocity.normalized * Time.deltaTime * player.attachForceMag);
+
+            shootRay.origin = camera.transform.position;
+            shootRay.direction = camera.transform.forward;
+
+            lineRenderer.SetPosition(0, shootRay.origin);
+            lineRenderer.SetPosition(1, shootRay.direction * RayLength);
+
+
+            if (Physics.Raycast(shootRay, out shootHit, RayLength, hookMask))
+            {
+                shootHit.transform.GetComponent<FixedJoint>().connectedBody = myRidgidbody;
+                //fixedJoint.connectedBody = myRidgidbody;
+            }
+
+
             //lineRenderer.enabled = true;
             //lineRenderer.SetPosition(0, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z));
             //lineRenderer.SetPosition(1, new Vector3(hookTarget.gameObject.transform.position.x, hookTarget.gameObject.transform.position.y, hookTarget.gameObject.transform.position.z));
@@ -58,7 +81,7 @@ public class Hook : MonoBehaviour
         // Set the hook on press and hold
         if (input.getSecondaryActionHold())
         {
-            fixedJoint.connectedBody = myRidgidbody;
+            //fixedJoint.connectedBody = myRidgidbody;
             
 
             //addedHindgeJoin.anchor = offset;
@@ -72,7 +95,7 @@ public class Hook : MonoBehaviour
         else if (!input.getSecondaryActionHold())
         {
             //Destroy(currentHindgeJoint);
-            Destroy(vine);
+            //Destroy(vine);
             fixedJoint.connectedBody = null;
         }
     }
